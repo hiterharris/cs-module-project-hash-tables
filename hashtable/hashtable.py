556326c02308e7f04b26b097b8a6142103ffda13
Hiter Harris <hiterharris@comcast.net>
2020-06-01 19:39:55 -0400

    refactor

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -1,13 +1,21 @@
 class HashTableEntry:
     """
-    Hash Table entry, as a linked list node.
+    Linked List hash table key/value pair
     """
-
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
 
+
+# Hash table can't have fewer than this many slots
+MIN_CAPACITY = 8
+
+def djb2(key):
+  hash = 5381
+  for c in key:
+    hash = (hash * 33) + ord(c)
+  return hash
 
 class HashTable:
     """
@@ -15,39 +23,57 @@ class HashTable:
     that accepts string keys
     Implement this.
     """
-    def __init__(self, capacity=8):
+
+    def __init__(self, capacity):
         self.capacity = capacity
-        self.storage = [None] * capacity  # [None, None, None...]
-        self.size = 0
+        self.data = [None] * capacity
+
+    def get_num_slots(self):
+        """
+        Return the length of the list you're using to hold the hash
+        table data. (Not the number of items stored in the hash table,
+        but the number of slots in the main list.)
+        One of the tests relies on this.
+        Implement this.
+        """
+        return self.capacity
+
+
+    def get_load_factor(self):
+        """
+        Return the load factor for this hash table.
+        Implement this.
+        """
+        # Your code here
+
 
     def fnv1(self, key):
         """
-        FNV-1 64-bit hash function
+        FNV-1 Hash, 64-bit
         Implement this, and/or DJB2.
         """
-        hash = 14695981039346656037
-        prime = 1099511628211
 
-        key_bytes = key.encode()
+        # Your code here
 
-        for byte in key_bytes:
-            hash ^= byte
-            hash *= prime
-        return hash
 
     def djb2(self, key):
         """
-        DJB2 32-bit hash function
+        DJB2 hash, 32-bit
         Implement this, and/or FNV-1.
         """
+        hash = 5381
+        for c in key:
+            hash = (hash * 33) + ord(c)
+        return hash
+
 
     def hash_index(self, key):
         """
         Take an arbitrary key and return a valid integer index
         between within the storage capacity of the hash table.
         """
-        return self.fnv1(key) % self.capacity
-        # return self.djb2(key) % self.capacity
+        #return self.fnv1(key) % self.capacity
+        return self.djb2(key) % self.capacity
 
     def put(self, key, value):
         """
@@ -56,21 +82,7 @@ class HashTable:
         Implement this.
         """
         index = self.hash_index(key)
-        entry = HashTableEntry(key, value)
-        if self.storage[index] is None:
-            self.size += 1
-            self.storage[index] = entry
-            self.resize()
-            return
-        node = self.storage[index]
-        while node.next is not None and node.key != key:
-            node = node.next
-        if node.key == key:
-            node.value = value
-            return
-        self.size += 1
-        node.next = entry
-        self.resize()
+        self.data[index] = value
 
 
     def delete(self, key):
@@ -80,23 +92,11 @@ class HashTable:
         Implement this.
         """
         index = self.hash_index(key)
-        node = self.storage[index]
-        if node is None:
-            return
-        previous_node = None
-        while node is not None:
-            if node.key == key:
-                if previous_node is None:
-                    self.storage[index] = node.next
-                    self.size -= 1
-                    return
-                else:
-                    previous_node.next = node.next
-                    self.size -= 1
-                    return
-            else:
-                previous_node = node
-                node = node.next
+        if self.data[index] == None:
+            print("Error: Key not found")
+        else:
+            self.data[index] = None
+
 
     def get(self, key):
         """
@@ -105,60 +105,51 @@ class HashTable:
         Implement this.
         """
         index = self.hash_index(key)
-        if self.storage[index] is None:
-            return None
-        node = self.storage[index]
-        while node.key != key:
-            if node.next is None:
-                return None
-            node = node.next
-        return node.value
+        value = self.data[index]
+        return value
 
-    def resize(self):
+
+    def resize(self, new_capacity):
         """
-        Doubles the capacity of the hash table and
-        rehash all key/value pairs.
+        Changes the capacity of the hash table and
+        rehashes all key/value pairs.
         Implement this.
         """
-        if self.size / self.capacity <= 0.7:
-            return
-        actual_size = self.size
-        self.capacity *= 2
-        old_storage = self.storage.copy()
-        self.storage = [None] * self.capacity
-        for node in old_storage:
-            if node is None:
-                continue
-            while node.next is not None:
-                self.put(node.key, node.value)
-                node = node.next
-            self.put(node.key, node.value)
-            self.size = actual_size
+        # Your code here
+
+
 
 if __name__ == "__main__":
+    ht = HashTable(8)
 
-    ht = HashTable(2)
+    ht.put("line_1", "'Twas brillig, and the slithy toves")
+    ht.put("line_2", "Did gyre and gimble in the wabe:")
+    ht.put("line_3", "All mimsy were the borogoves,")
+    ht.put("line_4", "And the mome raths outgrabe.")
+    ht.put("line_5", '"Beware the Jabberwock, my son!')
+    ht.put("line_6", "The jaws that bite, the claws that catch!")
+    ht.put("line_7", "Beware the Jubjub bird, and shun")
+    ht.put("line_8", 'The frumious Bandersnatch!"')
+    ht.put("line_9", "He took his vorpal sword in hand;")
+    ht.put("line_10", "Long time the manxome foe he sought--")
+    ht.put("line_11", "So rested he by the Tumtum tree")
+    ht.put("line_12", "And stood awhile in thought.")
 
-    ht.put("line_1", "Tiny hash table")
-    ht.put("line_2", "Filled beyond capacity")
-    ht.put("line_3", "Linked list saves the day!")
     print("")
 
     # Test storing beyond capacity
-    print(ht.get("line_1"))
-    print(ht.get("line_2"))
-    print(ht.get("line_3"))
+    for i in range(1, 13):
+        print(ht.get(f"line_{i}"))
 
     # Test resizing
-    old_capacity = len(ht.storage)
-    ht.resize()
-    new_capacity = len(ht.storage)
+    old_capacity = ht.get_num_slots()
+    ht.resize(ht.capacity * 2)
+    new_capacity = ht.get_num_slots()
 
     print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
     # Test if data intact after resizing
-    print(ht.get("line_1"))
-    print(ht.get("line_2"))
-    print(ht.get("line_3"))
+    for i in range(1, 13):
+        print(ht.get(f"line_{i}"))
 
     print("")
