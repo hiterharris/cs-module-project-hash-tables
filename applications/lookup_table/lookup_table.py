1e392c1e2898b16a34759f482279f5737deec1b8
Hiter Harris <hiterharris@comcast.net>
2020-06-03 14:14:48 -0400

    lookup_table

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,5 +1,7 @@
-# Your code here
+import math
+import random
 
+cache = {}
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -14,9 +16,12 @@ def slowfun(x, y):
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
+    v = math.pow(x, y)
+    v = math.factorial(v)
+    v //= (x + y)
+    v %= 982451653
 
-
+    return v
 
 # Do not modify below this line!
 
