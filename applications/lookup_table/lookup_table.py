bdc61d02866f9a495aaa31e26761d3d4edf93eba
Hiter Harris <hiterharris@comcast.net>
2020-06-04 09:14:11 -0400

    MVP

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,7 +1,8 @@
-import math
-import random
+import math, random
 
-cache = {}
+results = dict()
+powers = dict()
+factorials = dict()
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -11,18 +12,52 @@ def slowfun_too_slow(x, y):
 
     return v
 
-def slowfun(x, y):
+def slowfun_v1(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    v = math.pow(x, y)
-    v = math.factorial(v)
-    if x and y not in cache: # if n is not a key in cache
-        cache[x, y] = x + y
-    v %= 982451653
 
-    return v
+    if (x, y) in results:
+        return results[(x, y)]
+
+    if (x, y) in powers:
+        power = powers[(x, y)]
+    else:
+        power = math.pow(x, y)
+
+    if power in factorials:
+        factorial = factorials[power]
+    else:
+        factorial = math.factorial(power)
+
+    result = factorial // (x + y)
+    result %= 982451653
+
+    results[(x, y)] = result
+
+    return result
+
+# because there are only 36 combinations, compute them ahead of time
+precomputed_values = dict()
+
+for x in range(2, 14):
+
+    for y in range(3, 6):
+
+        result = math.pow(x, y)
+        result = math.factorial(result)
+        result //= (x + y)
+        result %= 982451653
+
+        precomputed_values[(x, y)] = result
+
+    print("stored all possibilities for x = ", x)
+
+def slowfun(x, y):
+
+    return precomputed_values[(x, y)]
+    
 
 # Do not modify below this line!
 
--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -3,13 +3,86 @@ find all a, b, c, d in q such that
 f(a) + f(b) = f(c) - f(d)
 """
 
-#q = set(range(1, 10))
-#q = set(range(1, 200))
+# q = set(range(1, 10))
+# q = set(range(1, 200))
 q = (1, 3, 4, 7, 12)
-
 
 def f(x):
     return x * 4 + 6
 
-# Your code here
+function_values = dict()
 
+# store known sums and differences and the tuples that will generate them
+sums_to_known_tuples = dict()
+differences_to_known_tuples = dict()
+
+for number1 in q:
+
+    # if needed, compute and store f(number1)
+    if number1 not in function_values:
+        function_values[number1] = f(number1)
+
+    for number2 in q:
+        
+        # if needed, compute and store f(number2)
+        if number2 not in function_values:
+            function_values[number2] = f(number2)
+
+        # use a variable to keep track of the tuple
+        tuple_1_2 = (number1, number2)
+        tuple_2_1 = (number2, number1)
+
+        # compute sums and differences
+        pair_sum = function_values[number1] + function_values[number2]
+        pair_difference = function_values[number1] - function_values[number2]
+
+        # store the pairs that will generate each sum
+        if pair_sum not in sums_to_known_tuples:
+            sums_to_known_tuples[pair_sum] = set()
+        
+        sums_to_known_tuples[pair_sum].add(tuple_1_2)
+        sums_to_known_tuples[pair_sum].add(tuple_2_1)
+
+        # store the pair for difference f(number1) - f(number2)
+        if pair_difference not in differences_to_known_tuples:
+            differences_to_known_tuples[pair_difference] = set()
+
+        differences_to_known_tuples[pair_difference].add(tuple_1_2)
+
+        # store the pair for difference f(number2) - f(number1)
+        if -pair_difference not in differences_to_known_tuples and number1 != number2:
+            differences_to_known_tuples[-pair_difference] = set()
+
+        differences_to_known_tuples[-pair_difference].add(tuple_2_1)
+
+# check for all values where the sum and difference are the same
+shared_values = set(sums_to_known_tuples.keys()).intersection(set(differences_to_known_tuples.keys()))
+
+def print_solutions(show_all_possibilities=True):
+
+    # count how many solutions were found
+    solutions = 0
+
+    # look up all tuple combinations for each sum and difference
+    for value in shared_values:
+
+        known_sum_tuples = sums_to_known_tuples[value]
+        known_difference_tuples = differences_to_known_tuples[value]
+
+        solutions += len(known_sum_tuples) * len(known_difference_tuples)
+
+        if show_all_possibilities:
+
+            for sum_tuple in known_sum_tuples:
+
+                for difference_tuple in known_difference_tuples:
+
+                    a, b = sum_tuple
+                    c, d = difference_tuple
+
+                    print(f"f({a}) + f({b}) = f({c}) - f({d})    {function_values[a]} + {function_values[b]} = {function_values[c]} - {function_values[d]}")
+
+    print("\n", solutions, "solutions found for q =", q)
+
+print_solutions()
+# print_solutions(False)
